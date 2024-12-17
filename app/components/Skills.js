446e1d3e078ade6bf34b2de6212aacbb274c1eea
albinthomas724 <albinthomas724@gmail.com>
2024-12-17 13:07:15 +0530

    made the shills responsive

--- app/components/Skills.js
+++ app/components/Skills.js
@@ -1,36 +1,31 @@
-import React from 'react'
-import Card from './Card'
+import React from 'react';
+import Card from './Card';
 import { FaReact } from "react-icons/fa";
 import { SiMongodb } from "react-icons/si";
 import { AiOutlineDotNet } from "react-icons/ai";
 import { FaJava } from "react-icons/fa";
 
-
 const Skills = () => {
   return (
-    <div className="flex flex-row  gap-4 justify-center">
+    <div className="flex flex-wrap gap-4 justify-center md:justify-between lg:justify-center px-4">
       <Card
         Icon={FaReact}
         title="React"
-       
       />
       <Card
         Icon={SiMongodb}
         title="Mongo DB"
-        
       />
       <Card
-        Icon={AiOutlineDotNet }
+        Icon={AiOutlineDotNet}
         title="DotNet"
-        
       />
       <Card
-        Icon={FaJava }
+        Icon={FaJava}
         title="Java"
-        
       />
     </div>
-  )
-}
+  );
+};
 
-export default Skills
+export default Skills;
